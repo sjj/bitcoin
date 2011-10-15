a9e526f940652a9846c39211422c209e1073bb3e
Suja <sujajss@gmail.com>
2011-10-15 18:29:37 +0530

    set AVG instead of AVG

--- app/controllers/ticker_controller.rb
+++ app/controllers/ticker_controller.rb
@@ -1,5 +1,5 @@
 class TickerController < ApplicationController
   def index
-    @bitcoinIndices = BitcoinIndex.select("date,AVG(usd) as usd,AVG(eur) as eur").group("date").order("date")
+    @bitcoinIndices = BitcoinIndex.select("date,AVG(usd) as usd,AVG(eur) as eur,date as dt").group("date").order("date")
   end
 end
