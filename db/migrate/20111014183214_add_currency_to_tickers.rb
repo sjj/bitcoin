115511ba20c6f15aadc4d6c261c02cbec97845c9
Suja <sujajss@gmail.com>
2011-10-15 19:07:21 +0530

    set AVG instead of AVG

--- db/migrate/20111014183214_add_currency_to_tickers.rb
+++
@@ -1,9 +0,0 @@
-class AddCurrencyToTickers < ActiveRecord::Migration
-  def self.up
-    add_column :tickers, :currency, :string
-  end
-
-  def self.down
-    remove_column :tickers, :currency
-  end
-end
